67b1951c7017af4dff92da9f0e06d89e628e685d
PjoterC <piotr.the.plane@gmail.com>
2025-12-29 17:47:03 +0100

    Created first testing file and the initial database entry reset file

---
+++ tests/walletreset.go
@@ -0,0 +1,24 @@
+package tests
+
+import (
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+// overbalance_test - testSourceA, testSourceB
+func ResetTestWallets() {
+	db, _ := sqlx.Open("postgres", "postgres://user:password@localhost:5432/btp_tokens?sslmode=disable")
+	defer db.Close()
+
+	db.MustExec(`
+		INSERT INTO wallets (address, balance) 
+		VALUES ('testSourceA', 1000)
+		ON CONFLICT (address) DO UPDATE SET balance = EXCLUDED.balance;
+
+		INSERT INTO wallets (address, balance) 
+		VALUES ('testDestA', 0)
+		ON CONFLICT (address) DO UPDATE SET balance = EXCLUDED.balance;
+	`)
+
+	println("Test wallets reset")
+}
